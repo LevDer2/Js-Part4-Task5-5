090f56e3c4b57fd7d8f9647662089924a2794b7d
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-09-06 14:40:57 +0300

    aaaa

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log("hello World");
+import { alert, defaultModules, success } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import * as PNotifyMobile from '@pnotify/mobile';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+
+defaultModules.set(PNotifyMobile, {});
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    success({
+        title: 'Дякую!',
+        text: 'Заявка принята!',
+        delay: 1000,
+    });
+});
